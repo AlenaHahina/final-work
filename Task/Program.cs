a878b9e76e823a2754bf13924762954d0c0ec142
AlenaHahina <alenaizmaylova.94@mail.ru>
2024-01-17 10:46:21 +0300

    Убрали лишнее присвоение переменной

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,6 +1,6 @@
 ﻿string[] array1 = new string[4] {"Hello", "2", "world", ":-"};
 string[] array2 = new string[array1.Length];
-int count = 0;
+
 void SecondArrayWithIF(string[] array1, string[] array2)
 {
     int count = 0;
